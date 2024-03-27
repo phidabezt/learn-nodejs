f33b1f5b5b1de1c8a71b2f1b2e094b9ec63b0302
Phi Nguyen <quangphi1231@gmail.com>
2024-03-27 22:56:06 +0700

    add template engine

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8888
 const hostname = process.env.HOST_NAME
 
+
+// template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// route
 app.get('/', (req, res) => {
   res.send('Hello World 2!')
+})
+
+app.get('/ejs', (req, res) => {
+  res.render('sample')
 })
 
 app.listen(port, hostname, () => {
