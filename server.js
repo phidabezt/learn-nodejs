89c4759eb2b2d028849105ad075616cae49c5902
Phi Nguyen <quangphi1231@gmail.com>
2024-03-27 22:13:09 +0700

    add nodemon

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8888
+const hostname = process.env.HOST_NAME
+
+app.get('/', (req, res) => {
+  res.send('Hello World 2!')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port ${port}`)
+})
